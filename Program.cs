2dc797fb06b24c1f34614ac50723bc02ed19c6a8
Stas Igolni4kin <stam1989@mail.ru>
2022-04-07 21:34:39 +0300

    add files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Personality test
+            //приложение просит пользователя ввести его любимый цвет
+            //на основе введённого значения, консоль показывает пользователя некоторую обратную связь
+            //red - you are so romantic
+            //blue - you are workaholic
+            //grenn - you care about the enviroment
+            //любое другое значение, то консоль выведет предложение " you are a {userInput} unicorn"
+
+            Console.WriteLine("Please enter your favorit color");
+            string userInput = Console.ReadLine().ToLower(); //RED/Red/reD/rEd/rdE --> red
+
+            if(userInput == "red")
+            {
+                Console.WriteLine("You are so romantic.");
+            }
+            
+                else if(userInput == "blue")
+                {
+                    Console.WriteLine("you are workaholic.");
+                }
+            else if (userInput == "green")
+            {
+                Console.WriteLine("you care about the enviroment.");
+
+
+            }
+            {
+                Console.WriteLine($"You are {userInput} unicorn");
+            }
+            Console.WriteLine("Have a nice day");
+            }
+        }
+    }
+
